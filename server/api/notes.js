acec074980a48e431ed490f0a4b0d391dc011555
Admin <drew24n@gmail.com>
2020-12-11 17:11:28 +0200

    minor server refactoring

--- server/api/notes.js
+++ server/api/notes.js
@@ -1,106 +1,95 @@
-const app = require('../server')
+const {Router} = require('express')
 const Notes = require('../models/notes')
 
-const notesApi = {
-    post() {
-        return app.post('/api/notes', async (req, res) => {
-            try {
-                const note = {
-                    title: req.body.title,
-                    text: req.body.text
-                }
+const notesRoutes = Router()
 
-                const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
+notesRoutes.post('/api/notes', async (req, res) => {
+    try {
+        const note = {
+            title: req.body.title,
+            text: req.body.text
+        }
 
-                function addImage(note, imageEncoded) {
-                    if (!imageEncoded) return
-                    if (imageEncoded && imageMimeTypes.includes(imageEncoded.type)) {
-                        note.image = new Buffer.from(imageEncoded.data, 'base64')
-                        note.imageType = imageEncoded.type
-                    }
-                }
+        const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
 
-                addImage(note, req.body.image)
+        function addImage(note, imageEncoded) {
+            if (!imageEncoded) return
+            if (imageEncoded && imageMimeTypes.includes(imageEncoded.type)) {
+                note.image = new Buffer.from(imageEncoded.data, 'base64')
+                note.imageType = imageEncoded.type
+            }
+        }
 
-                const newNote = await Notes.create(note)
-                res.status(201).json({success: true, data: newNote})
-            } catch (error) {
-                res.status(400).json({success: false, error: error.message})
-            }
-        })
-    },
-    get() {
-        return app.get('/api/notes', async (req, res) => {
-            if (req.query.id) {
-                try {
-                    const note = await Notes.findById(req.query.id)
-                    if (note) {
-                        res.status(200).json({success: true, data: note})
-                    } else if (!note) {
-                        res.status(400).json({success: false, error: 'Note was not found'})
-                    }
-                } catch (error) {
-                    res.status(400).json({success: false, error: error.message})
-                }
-            } else if (req.query.latest) {
-                try {
-                    const note = await Notes.find().sort({date: -1}).limit(1)
-                    if (note) {
-                        res.status(200).json({success: true, data: note})
-                    } else if (!note) {
-                        res.status(400).json({success: false, error: 'Note was not found'})
-                    }
-                } catch (error) {
-                    res.status(400).json({success: false, error: error.message})
-                }
-            } else if (req.query.page) {
-                try {
-                    const page = req.query.page ? parseInt(req.query.page) : 1
-                    const size = req.query.size ? parseInt(req.query.size) : 3
-                    const notes = await Notes.find()
-                        .skip((page - 1) * size)
-                        .limit(size)
-                    const totalCount = await Notes.countDocuments()
-                    res.status(200).json({success: true, data: {notes, totalCount}})
-                } catch (error) {
-                    res.status(400).json({success: false, error: error.message})
-                }
-            }
-        })
-    },
-    delete() {
-        return app.delete('/api/notes', async (req, res) => {
-            try {
-                const note = await Notes.deleteOne({_id: req.query.id})
-                if (note.n) {
-                    res.status(200).json({success: true, data: {}})
-                } else if (!note.n) {
-                    res.status(400).json({success: false, error: 'Note was not found. Nothing to delete'})
-                }
-            } catch (error) {
-                res.status(400).json({success: false, error: error.message})
-            }
-        })
-    },
-    put() {
-        return app.put('/api/notes', async (req, res) => {
-            try {
-                const note = await Notes.findByIdAndUpdate(req.query.id, req.body, {new: true, runValidators: true})
-                if (note) {
-                    res.status(201).json({success: true, data: note})
-                } else if (!note) {
-                    res.status(400).json({success: false, error: 'Note was not found. Nothing to updated'})
-                }
-            } catch (error) {
-                res.status(400).json({success: false, error: error.message})
-            }
-        })
+        addImage(note, req.body.image)
+
+        const newNote = await Notes.create(note)
+        res.status(201).json({success: true, data: newNote})
+    } catch (error) {
+        res.status(400).json({success: false, error: error.message})
     }
-}
+})
+notesRoutes.get('/api/notes', async (req, res) => {
+    if (req.query.id) {
+        try {
+            const note = await Notes.findById(req.query.id)
+            if (note) {
+                res.status(200).json({success: true, data: note})
+            } else if (!note) {
+                res.status(400).json({success: false, error: 'Note was not found'})
+            }
+        } catch (error) {
+            res.status(400).json({success: false, error: error.message})
+        }
+    } else if (req.query.latest) {
+        try {
+            const note = await Notes.find().sort({date: -1}).limit(1)
+            if (note) {
+                res.status(200).json({success: true, data: note})
+            } else if (!note) {
+                res.status(400).json({success: false, error: 'Note was not found'})
+            }
+        } catch (error) {
+            res.status(400).json({success: false, error: error.message})
+        }
+    } else if (req.query.page) {
+        try {
+            const page = req.query.page ? parseInt(req.query.page) : 1
+            const size = req.query.size ? parseInt(req.query.size) : 3
+            const notes = await Notes.find()
+                .skip((page - 1) * size)
+                .limit(size)
+            const totalCount = await Notes.countDocuments()
+            res.status(200).json({success: true, data: {notes, totalCount}})
+        } catch (error) {
+            res.status(400).json({success: false, error: error.message})
+        }
+    } else {
+        res.status(400).json({success: false, error: 'An error occurred'})
+    }
+})
+notesRoutes.delete('/api/notes', async (req, res) => {
+    try {
+        const note = await Notes.deleteOne({_id: req.query.id})
+        if (note.n) {
+            res.status(200).json({success: true, data: {}})
+        } else if (!note.n) {
+            res.status(400).json({success: false, error: 'Note was not found. Nothing to delete'})
+        }
+    } catch (error) {
+        res.status(400).json({success: false, error: error.message})
+    }
+})
+notesRoutes.put('/api/notes', async (req, res) => {
+    try {
+        const note = await Notes.findByIdAndUpdate(req.query.id, req.body, {new: true, runValidators: true})
+        if (note) {
+            res.status(201).json({success: true, data: note})
+        } else if (!note) {
+            res.status(400).json({success: false, error: 'Note was not found. Nothing to updated'})
+        }
+    } catch (error) {
+        res.status(400).json({success: false, error: error.message})
+    }
+})
 
-notesApi.post()
-notesApi.get()
-notesApi.delete()
-notesApi.put()
-
-module.exports = notesApi
+module.exports = notesRoutes
--- server/server.js
+++ server/server.js
@@ -2,7 +2,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
+const notesRoute = require('./api/notes')
+
 const {env: {MONGO_URI}} = require('./config')
+const whiteList = ['https://blog-notes.netlify.app', 'http://localhost:3000']
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json({limit: '10mb', extended: false}))
+app.use(cors({credentials: true, origin: whiteList}))
+app.use(notesRoute)
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
@@ -10,19 +20,7 @@ mongoose.connect(MONGO_URI, {
     useCreateIndex: true,
     useFindAndModify: false
 })
-    .then(() => console.log('database connected'))
+    .then(() => {
+        app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+    })
     .catch(error => console.log(error))
-
-const app = express()
-
-const whiteList = ['https://blog-notes.netlify.app', 'http://localhost:3000']
-
-app.use(express.json({limit: '10mb', extended: false}))
-app.use(cors({credentials: true, origin: whiteList}))
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
-
-module.exports = app
-
-require('./api/notes')
